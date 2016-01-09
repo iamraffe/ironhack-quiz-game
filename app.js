b35f8dbae8bd2b64f1595d31a45fa0eda6f01ffe
Rafa Ramírez <raffe90@gmail.com>
2016-01-09 17:26:23 +0100

    Second & Third Iteration

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var read = require('read');
 
-var Question = function (id, text, answer){
+var Question = function (id, text, answer, points){
   this.id = id;
   this.text = text;
   this.answer = answer;
+  this.points = points;
 }
 
 var Quiz = function (player, questions) {
@@ -11,6 +12,8 @@ var Quiz = function (player, questions) {
   this.player = player;
   this.current_question = this.questions.shift();
   this.game_over = false;
+  this.total = 0;
+  this.bonus_id = Math.floor(Math.random()*questions.length);
 
   this.add_question = function (question) {
     this.questions.push(question);
@@ -18,25 +21,43 @@ var Quiz = function (player, questions) {
 
   this.correct_response = function () {
     console.log("Respuesta correcta!");
+    this.total += this.current_question.points;
     this.next_question();
   }
 
+  this.questions_left = function () {
+    // console.log("Q length", this.questions);
+    return this.questions.length > 0;
+  }
+
   this.next_question = function () {
-    this.current_question = this.questions.shift();
-    if(!this.game_over){
-      this.ask();
+    if(this.questions_left()){
+      this.current_question = this.questions.shift();
+      if(!this.game_over){
+        this.ask();
+      }
+    }
+    else{
+      console.log("END OF QUIZ");
     }
   }
 
   this.incorrect_response = function () {
     console.log("Incorrecto");
+    this.total -= this.current_question.points;
     this.ask();
   }
 
   this.ask = function () {
-    var _question = this.current_question;
+    var _question = this.current_question.text + " Points: " + this.current_question.points;
+
+    if(this.current_question.id === this.bonus_id){
+      _question += " !!BONUS QUESTION!! "
+      this.current_question.points *= 2;
+    }
+
     options = {
-        prompt: this.current_question.text
+        prompt: _question
     }
 
     read(options, check_answer);
@@ -52,9 +73,10 @@ var Quiz = function (player, questions) {
   }
 }
 
-var q1 = new Question(1, "Es esto una pregunta?", "Si");
-var q2 = new Question(2, "Es esto otra pregunta?", "No");
-var quiz = new Quiz("R", [q1, q2]);
+var q1 = new Question(1, "Es esto una pregunta?", "Si", 45);
+var q2 = new Question(2, "Es esto otra pregunta?", "No", 45);
+var q3 = new Question(3, "De que color es el caballo blanco de Bolivar?", "Blanco", 15);
+var quiz = new Quiz("R", [q1, q2, q3]);
 
 quiz.ask();
 
